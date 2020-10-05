e9532d5de9ee4f9f02df67a623896ff56091293e
unknown <eslaminasrin60@gmail.com>
2020-10-05 10:21:13 -0700

    create list&listnode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
+import "./styles/style.css";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      data: [],
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    fetch("https://coronavirus-19-api.herokuapp.com/countries")
+      .then((res) => res.json())
+      .then((response) => this.setState({ data: response, loading: false }))
+      .catch((err) => console.log(err));
+  }
+  // async fetchData() {
+  //   let data = await fetch("https://api.covid19api.com/countries");
+  //   console.log(data.json());
+  // }
+
   render() {
-    return <div></div>;
+    console.log(this.state.data);
+    if (this.state.loading === true) {
+      return <div>loading</div>;
+    } else {
+      return (
+        <div>
+          <Header />
+        </div>
+      );
+    }
   }
 }
